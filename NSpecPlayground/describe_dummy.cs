4c3599111cb527b76bdd0bc5a064c58916df9f70
Rumen Paletov <rumen.paletov@emerchantpay.com>
2014-11-18 10:27:13 +0200

    Introduce NSpecPlayground sln

---
+++ NSpecPlayground/describe_dummy.cs
@@ -0,0 +1,19 @@
+﻿using NSpec;
+using System;
+using System.Linq;
+
+namespace NSpecPlayground
+{
+    public class describe_dummy : nspec
+    {
+        void it_should_pass()
+        {
+            true.should_be_true();
+        }
+
+        void it_should_fail()
+        {
+            true.should_be_false();
+        }
+    }
+}
